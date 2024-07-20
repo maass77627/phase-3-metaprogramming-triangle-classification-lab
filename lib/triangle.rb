89dac2d28220a7e55d95eda6c78b435ad6d313c4
kelly_maass <kmaass77627@gmail.com>
2024-07-19 20:57:26 -0500

    done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
+require 'pry'
+
 class Triangle
-  # write code here
+ attr_accessor :one, :two, :three
+
+def initialize(one, two, three)
+  @one = one
+  @two = two
+  @three = three
+end
+
+def kind()
+  # binding.pry
+  if @one < 0 || @two < 0 || @three < 0
+    raise TriangleError
+  elsif @one + @two <= @three || @one + @three <= @two || @two + @three <= @one
+    raise TriangleError
+elsif @one == @two && @two == @three
+  return :equilateral
+elsif @one == @three && @one != @two || @one == @two && @one != @three || @two == @three && @two != @one
+  return :isosceles
+elsif @one != @two && @two != @three
+  return :scalene
+ 
+else 
+  return false
+end
+end
+
+
+
+
+class TriangleError < StandardError
+  def message
+    puts "triangle not valid"
+  end
+end
+
+
+
+
 end
